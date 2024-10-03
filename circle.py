4743cc47241e8a0655db1c9bcac642d01edd1fd1
baglalus <noscope360bol@gmail.com>
2024-10-03 15:44:56 +0300

    docks added

--- circle.py
+++ circle.py
@@ -2,9 +2,13 @@ import math
 
 
 def area(r):
+    #	r - circle radius
+    #	returns area of a circle
     return math.pi * r * r
 
 
 def perimeter(r):
+    #	r - circle radius 
+    #	returns perimeter of a circle
     return 2 * math.pi * r
 
--- square.py
+++ square.py
@@ -1,7 +1,12 @@
 
 def area(a):
+    #	a - side of a square
+    #	returns area of a square
     return a * a
 
 
 def perimeter(a):
+    #	a - side of a square
+    #	returns perimeter of a square
     return 4 * a
+
