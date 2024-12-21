fa0cc0d8b2c458fe2f21acb29fe41ae177e64295
Bogdan Karayanidi <67959340+Baglalus@users.noreply.github.com>
2024-12-21 09:35:50 +0300

    Revert "docks added"

--- circle.py
+++ circle.py
@@ -2,13 +2,9 @@ import math
 
 
 def area(r):
-    #	r - circle radius
-    #	returns area of a circle
     return math.pi * r * r
 
 
 def perimeter(r):
-    #	r - circle radius 
-    #	returns perimeter of a circle
     return 2 * math.pi * r
 
